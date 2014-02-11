7468af43fd89e9df6e0399022859be8b3f8f09b1
gagarinbefree <gagarinbefree@gmail.com>
2014-02-11 17:21:28 +0600

    add DrawingPrinter  module

--- ProfileCut/ModuleConnect/HardwareInterfaces.cs
+++ ProfileCut/ModuleConnect/HardwareInterfaces.cs
@@ -15,17 +15,11 @@ namespace HardwareInterfaces
     {
         void SetFont(string name, int size);
 
-        void WriteText(RectangleF bounds, int horAlign, int verAlign, string text);
+        void WriteText(RectangleF bounds, int horAlign, int verAlign, string text, float angle);
 
         void WriteBarcode(PointF origin, float height, int horAlign, int verAlign, string text);
 
-        //void WriteText(string text, double x, double y, double angle, int align, double width);
-        //void WriteText(string text, double x, double y, int alignHor, int alignVer, double angle, double maxWidth, double maxHeight, bool doShrink, bool doGrow);
-        //void DrawLine(float x1, float y1, float x2, float y2);
-        //void DrawLine(Point p1, Point p2);
-        //void WriteBarcode(string text, double x, double y, double height, double width);
-        //void NewPage(double width, double height);
-        void NewPage(double width, double height, int fieldLeft, int fieldTop, int fieldRight, int filedBottom);
+        void NewPage(float width, float height, int fieldLeft, int fieldTop, int fieldRight, int filedBottom);
         void Init(string printerName);
     }
 }
