89c9bd2b65b98729fd91411da9eec213de964a18
Roman86 <romych.sync@gmail.com>
2014-04-30 18:05:25 +0600

    untracked

---
+++ ProfileCut/ProfileCut/RAppCommandButton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Windows.Forms;
+using System.Reflection;
+
+using System.ComponentModel.Design.Serialization;
+
+namespace ProfileCut
+{
+	public class RAppCommandButton : Button
+	{
+		public RAppCommand appCommand;
+		public RAppCommandButton(RAppCommand cmd)
+		{
+			Text = cmd.Text;
+			this.appCommand = cmd;
+		}
+	}
+}
